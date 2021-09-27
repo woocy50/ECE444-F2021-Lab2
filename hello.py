21b94e9d7e233ab492a2fef87a110f7d59dc9016
Nick Woo <nicholas.woo@mail.utoronto.ca>
2021-09-26 22:09:43 -0400

    example 4-5 done

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'NickIsTheBest'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
@@ -19,12 +19,15 @@ class NameForm(Form):
 def index():
 #    return '<h1>Hello World!</h1>'
 #    return render_template('index.html', current_time=datetime.utcnow())
-    name = None
+#    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+#        name = form.name.data
+        session['name'] = form.name.data
+#        form.name.data = ''
+        return redirect(url_for('index'))
+#    return render_template('index.html', form=form, name=name)
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
