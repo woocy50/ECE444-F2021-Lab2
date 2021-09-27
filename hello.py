c67783366034844de3442d8f1411bce7dc3b2948
Nick Woo <nicholas.woo@mail.utoronto.ca>
2021-09-26 22:26:08 -0400

    Activity 4 done

--- hello.py
+++ hello.py
@@ -13,23 +13,24 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-#    return '<h1>Hello World!</h1>'
-#    return render_template('index.html', current_time=datetime.utcnow())
-#    name = None
     form = NameForm()
     if form.validate_on_submit():
-#        name = form.name.data
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
+        if 'utoronto' in form.email.data:
+            flash(f'Your name is: {form.name.data}<br>Your email is: {form.email.data}')
+        else:
+            flash('Please enter a UofT email')
+
         session['name'] = form.name.data
-#        form.name.data = ''
         return redirect(url_for('index'))
-#    return render_template('index.html', form=form, name=name)
     return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
