2702431de8979e1d400608c0d64a21b44b4a95d0
Nick Woo <nicholas.woo@mail.utoronto.ca>
2021-09-26 22:01:44 -0400

    trying to move onto example 4 series...

--- hello.py
+++ hello.py
@@ -2,15 +2,29 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'NickIsTheBest'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
 #    return '<h1>Hello World!</h1>'
-    return render_template('index.html', current_time=datetime.utcnow())
+#    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
